d0646583f1b7807bd454a85bd179e03cb3695de5
shawn <shawn@aistechnolabs.org>
2025-09-04 17:05:14 +0530

    Added product image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>Product Details Page</h2>
+      <h1>Product Details Page</h1>
     </div>
   );
 }
